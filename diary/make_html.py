6a0ba87685501d994f23b782784f0708d02c66e5
pullmay <yasunari.hikima@gmail.com>
2020-10-30 13:01:13 +0900

    .

--- diary/make_html.py
+++ diary/make_html.py
@@ -1,21 +1,41 @@
 import sys
+from datetime import datetime, date, timedelta
+from dateutil.relativedelta import relativedelta
 
-def make_file(day: str):
+def make_file():
     with open('template.html', 'r') as file:
         html = file.read()
         
     # ファイルが存在しない場合のみ書き込みする
-    new_file = day + '.html'
+    today = datetime.today()
+    d_today = datetime.strftime(today, '%y%m%d')
+    new_file = d_today + '.html'
 
     try:
         with open(new_file, mode='x') as f:
+            # 日付
+            today = datetime.today()
+            yesterday = today - timedelta(days=1)
+            tomorrow = today + timedelta(days=1)
+            one_month_ago = today - relativedelta(months=1)
+            one_month_after = today + relativedelta(months=1)
+            # 6桁
+            d_today = datetime.strftime(today, '%y%m%d')
+            d_yesterday = datetime.strftime(yesterday, '%y%m%d')
+            d_tomorrow = datetime.strftime(tomorrow, '%y%m%d')
+            d_one_month_ago = datetime.strftime(one_month_ago, '%y%m%d')
+            d_one_month_after = datetime.strftime(one_month_after, '%y%m%d')
             # 置換
-            tar_1 = '20' + day[0:2] + '/' + day[2:4] + '/' + day[4:6]
-            tar_2 = str(int(day) - 1) + '.html'
-            tar_3 = str(int(day) + 1) + '.html'
+            tar_1 = d_today
+            tar_2 = d_one_month_ago + '.html'
+            tar_3 = d_yesterday + '.html'
+            tar_4 = d_tomorrow + '.html'
+            tar_5 = d_one_month_after + '.html'
             html = html.replace('target_1', tar_1)
             html = html.replace('target_2', tar_2)
             html = html.replace('target_3', tar_3)
+            html = html.replace('target_4', tar_4)
+            html = html.replace('target_5', tar_5)
             f.write(html)
     except FileExistsError:
         pass
@@ -23,8 +43,8 @@ def make_file(day: str):
 
 def main():
     try:
-        day = sys.argv[1]
-        make_file(day)
+        # day = sys.argv[1]
+        make_file()
     except IndexError:
         pass
 
